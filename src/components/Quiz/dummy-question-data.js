139f450bfcdd353ca61ee15a71cd9bdeff79c054
unknown <aneesh0224@gmail.com>
2023-03-15 23:27:05 +0530

    finished quiz app on new repo

---
+++ src/components/Quiz/dummy-question-data.js
@@ -0,0 +1,72 @@
+const data = {
+  physics: [
+    {
+      question:
+        "The parliament act was passed in the year ....................... .",
+      options: [1998, 1947, 1987, 1997],
+      correctAnswer: 0
+    },
+    {
+      question:
+        "The pssdent act dss passfd in tdfe ferear ....................... .",
+      options: ["djsk", "1947", "ddssd", "sjdisjd"],
+      correctAnswer: 1
+    },
+    {
+      question:
+        "The pssdent acdstcd dss padsdssfd insdsd tdasasfe faweweerear ....................... .",
+      options: ["3rd", "4th", "1987", "sdjsdh"],
+      correctAnswer: 2
+    },
+  ],
+  chemistry: [
+    {
+      question:
+        "4th quwst acdstcd dss padsdssfd insdsd tdasasfe faweweerear ....................... .",
+      options: ["3rd", "4th", "1987", "sdjsdh"],
+      correctAnswer: 3
+    },
+    {
+      question:
+        "5th wiasu hisdiuas idu asui dui asow nld oasjdo iosd dos jdcsid odincos doi dosdjos osd dos ....................... .",
+      options: [1998, 1947, 1987, 1997],
+      correctAnswer: 0
+    },
+  ],
+  maths: [
+    {
+      question:
+        "6th wiasu hisdiuas idu asui dui asow nld oasjdo iosd dos jdcsid odincos doisdsd dosdjos osd dos ....................... .",
+      options: ["dsushd", 1947, 1987, "sdjijsd"],
+      correctAnswer: 1
+    },
+
+    {
+      question:
+        "7th wiasu hdsdsd sds ssdsdisdiuas idu asui dui asow nld oasjdo iosd dos jdcsid odincos doisdsd dosdjos osd dos ....................... .",
+      options: ["dsushd", 1947, 1987, "sdjijsd"],
+      correctAnswer: 2
+    },
+
+    {
+      question:
+        "8th wiasu hsd dfdf dfdisdiuas idu asui dui asow nld oasjdo iosd dos jdcsid odincos doisdsd dosdjos osd dos ....................... .",
+      options: ["dsushd", "sdksd", "sdksdj", "sdjijsd"],
+      correctAnswer: 3
+    },
+    {
+      question:
+        "9th widsd ds ds dsasu hsd dfdf dfdisdiuas idu asui dui asow nld oasjdo iosd dos jdcsid odincos doisdsd dosdjos osd dos ....................... .",
+      options: ["dsushd", "sewedksd", "sdkdfdfsdj", "sdeweijsd"],
+      correctAnswer: 0
+    },
+    {
+      question:
+        "10th wiasdd fdf d fdf u hsd dfdf dfdisdiuas idu asui dui asow nld oasjdo iosd dos jdcsid odincos doisdsd dosdjos osd dos ....................... .",
+      options: ["dsushd", "fdfdf", "sdkdfdfsdj", "sdjijsd"],
+      correctAnswer: 1
+    },
+  ],
+};
+
+export default data;
